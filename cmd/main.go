4346714b943e1d2ebef554c541ad0306a14aaa6e
Teleфonчиk <matvey-sizov@mail.ru>
2023-06-01 15:31:07 +0300

    feat: create init type and func, closed #41

--- cmd/main.go
+++ cmd/main.go
@@ -4,20 +4,18 @@ import (
 	"context"
 	"fmt"
 	"github.com/gin-gonic/gin"
-	"github.com/gin-gonic/gin/binding"
-	"github.com/go-playground/validator/v10"
 	"github.com/redis/go-redis/v9"
 	"github.com/wtkeqrf0/you-together/ent"
 	"github.com/wtkeqrf0/you-together/internal/controller"
 	"github.com/wtkeqrf0/you-together/internal/repo/postgres"
 	redisRepo "github.com/wtkeqrf0/you-together/internal/repo/redis"
 	"github.com/wtkeqrf0/you-together/internal/service"
-	"github.com/wtkeqrf0/you-together/pkg/bind"
 	"github.com/wtkeqrf0/you-together/pkg/client/email"
 	"github.com/wtkeqrf0/you-together/pkg/client/postgresql"
 	redisInit "github.com/wtkeqrf0/you-together/pkg/client/redis"
 	"github.com/wtkeqrf0/you-together/pkg/conf"
 	"github.com/wtkeqrf0/you-together/pkg/log"
+	"github.com/wtkeqrf0/you-together/pkg/middleware/bind"
 	"github.com/wtkeqrf0/you-together/pkg/middleware/errs"
 	"github.com/wtkeqrf0/you-together/pkg/middleware/query"
 	"github.com/wtkeqrf0/you-together/pkg/middleware/session"
@@ -30,20 +28,14 @@ import (
 )
 
 func main() {
-	// global validator of incoming values
-	binding.Validator = bind.NewValid(validator.New())
-
 	cfg := conf.GetConfig()
 
 	pClient, rClient, mailClient := getClients(cfg)
 
-	h := initHandler(pClient, rClient, mailClient)
-	m := initMiddlewares()
-
+	h := initHandler(pClient, rClient, mailClient, cfg)
 	r := gin.New()
 
-	m.InitGlobalMiddleWares(r)
-	h.InitRoutes(r.Group(cfg.Listen.MainPath), mailClient != nil)
+	h.InitRoutes(createSetter(r, pClient, rClient, cfg, mailClient != nil))
 
 	run(cfg.Listen.Port, r, pClient, rClient, mailClient)
 }
@@ -87,11 +79,10 @@ func run(port int, r *gin.Engine, pClient *ent.Client, rClient *redis.Client, ma
 		log.WithErr(err).Fatal("PostgreSQL Connection Shutdown Failed")
 	}
 
+	log.Info("Server Exited Properly")
 	if err := mailClient.Quit(); err != nil {
 		log.WithErr(err).Fatal("Email Connection Shutdown Failed")
 	}
-
-	log.Info("Server Exited Properly")
 }
 
 func getClients(cfg *conf.Config) (*ent.Client, *redis.Client, *smtp.Client) {
@@ -105,10 +96,10 @@ func getClients(cfg *conf.Config) (*ent.Client, *redis.Client, *smtp.Client) {
 	return pClient, rClient, mailClient
 }
 
-func initHandler(pClient *ent.Client, rClient *redis.Client, mailClient *smtp.Client) *controller.Handler {
+func initHandler(pClient *ent.Client, rClient *redis.Client, mailClient *smtp.Client, cfg *conf.Config) *controller.Handler {
 	pUser := postgres.NewUserStorage(pClient.User)
 	rConn := redisRepo.NewRClient(rClient)
-	mailConn := service.NewEmailSender(mailClient)
+	mailSender := service.NewEmailSender(mailClient)
 
 	auth := service.NewAuthService(pUser, rConn)
 	user := service.NewUserService(pUser, rConn)
@@ -116,15 +107,24 @@ func initHandler(pClient *ent.Client, rClient *redis.Client, mailClient *smtp.Cl
 	return controller.NewHandler(
 		user,
 		auth,
-		mailConn,
-		session.NewAuth(auth),
-		bind.NewValid(validator.New()),
+		mailSender,
+		session.NewAuth(auth, cfg),
 	)
 }
 
-func initMiddlewares() *controller.Middlewares {
-	return controller.NewMiddleWares(
+func createSetter(r *gin.Engine, pClient *ent.Client, rClient *redis.Client, cfg *conf.Config, mailSet bool) *controller.Setter {
+	pUser := postgres.NewUserStorage(pClient.User)
+	rConn := redisRepo.NewRClient(rClient)
+
+	auth := service.NewAuthService(pUser, rConn)
+
+	return controller.NewSetter(
+		r,
+		bind.NewValidator(),
 		errs.NewErrHandler(),
 		query.NewQueryHandler(),
+		session.NewAuth(auth, cfg),
+		cfg.Listen.MainPath,
+		mailSet,
 	)
 }
